a6e89bb428d0b10c6f6fba2e9c6072042d087cbc
Tuan Truong <tuan188@gmail.com>
2024-11-13 13:27:54 +0700

    Update iOS requirement

--- CleanArchitecture/Package.swift
+++ CleanArchitecture/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CleanArchitecture",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CleanArchitecture",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v13)
     ],
     products: [
         .library(
